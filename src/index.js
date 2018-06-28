1554443c2621cdf1d72afaf18a2249664f58af0a
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-06-28 15:41:19 +0200

    fix: Move button to the end of the menu

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ export default class {
         app.addRouteListener('course.settings', function (params) {
             let url = '/courses/' + params.id + '/undelete';
             
-            app.addReadyListener('#right-side > div:first-child', function (el) {
-                el.insertAdjacentHTML('afterbegin', `
+            app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
+                el.insertAdjacentHTML('afterend', `
                     <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
                         <i class="icon-trash"></i>Course Recycle Bin
                     </a>
