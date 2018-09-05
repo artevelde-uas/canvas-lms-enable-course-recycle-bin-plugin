64b072a3096b81a089b6fb565f00b7619819b114
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-09-05 09:33:10 +0200

    Even better, export function instead of object

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import i18n from './i18n';
 
 
-export default {
-    
-    init(app) {
-        app.addRouteListener('course.settings', function (params) {
-            let url = '/courses/' + params.id + '/undelete';
-            
-            app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
-                el.insertAdjacentHTML('afterend', `
-                    <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
-                        <i class="icon-trash"></i>${i18n.t('recycle_bin')}
-                    </a>
-                `);
-            });
+export default function (app) {
+    app.addRouteListener('course.settings', function (params) {
+        let url = '/courses/' + params.id + '/undelete';
+        
+        app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
+            el.insertAdjacentHTML('afterend', `
+                <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
+                    <i class="icon-trash"></i>${i18n.t('recycle_bin')}
+                </a>
+            `);
         });
-    }
-    
+    });
 }
