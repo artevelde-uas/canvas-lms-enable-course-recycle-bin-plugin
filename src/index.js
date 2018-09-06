85d17a277ba3a3cfdf57e7c5d47537c756b720bb
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-09-06 07:14:54 +0200

    Add translations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+import i18next from 'i18next';
+
+const i18n = i18next.createInstance().init({
+    'lng': document.documentElement.lang,
+    resources: {
+        "en": {
+            "translation": {
+                "recycle_bin": "Course Recycle Bin"
+            }
+        },
+        "nl": {
+            "translation": {
+                "recycle_bin": "Cursusprullenbak"
+            }
+        },
+        "fr": {
+            "translation": {
+                "recycle_bin": "Corbeille du cours"
+            }
+        },
+        "de": {
+            "translation": {
+                "recycle_bin": "Kurs Papierkorb"
+            }
+        }
+    }
+});
+
 
 export default class {
     
@@ -8,7 +36,7 @@ export default class {
             app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
                 el.insertAdjacentHTML('afterend', `
                     <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
-                        <i class="icon-trash"></i>Course Recycle Bin
+                        <i class="icon-trash"></i>${i18n.t('recycle_bin')}
                     </a>
                 `);
             });
