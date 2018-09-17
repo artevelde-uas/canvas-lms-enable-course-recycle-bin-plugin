bc35b6bdcf3cf0c644ed05873736af2685ab19e2
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2018-09-17 09:57:37 +0200

    Change 'id' to 'courseId'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import i18n from './i18n';
 
 export default function (app) {
     app.addRouteListener('course.settings', function (params) {
-        let url = '/courses/' + params.id + '/undelete';
+        let url = '/courses/' + params.courseId + '/undelete';
         
         app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
             el.insertAdjacentHTML('afterend', `
