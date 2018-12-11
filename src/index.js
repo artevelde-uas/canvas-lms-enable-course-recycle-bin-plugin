bbf3eb9b3968cc684b557afba8ac514f46bed24b
Renaat De Muynck <renaat.demuynck@gmail.com>
2018-12-11 14:39:23 +0100

    Use the translator service provided by the Canvas app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import i18n from './i18n';
+import translations from './i18n.json';
 
 
 export default function (app) {
+    const __ = app.i18n.translate;
+    
+    app.i18n.setTranslations(translations);
+    
     app.addRouteListener('courses.settings', function (params) {
         let url = app.getRouteUrl('courses.settings.undelete', {
             courseId: params.courseId
@@ -10,7 +14,7 @@ export default function (app) {
         app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
             el.insertAdjacentHTML('afterend', `
                 <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
-                    <i class="icon-trash"></i>${i18n.t('recycle_bin')}
+                    <i class="icon-trash"></i>${__('recycle_bin')}
                 </a>
             `);
         });
