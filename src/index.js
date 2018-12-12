4a01fc978d0120861cee8a5c519295e405fcefb5
Renaat De Muynck <renaat.demuynck@gmail.com>
2018-12-12 13:48:59 +0100

    fix: Remove deprecated route name

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import i18n from './i18n';
 
 
 export default function (app) {
-    app.addRouteListener('course.settings', function (params) {
+    app.addRouteListener('courses.settings', function (params) {
         let url = '/courses/' + params.courseId + '/undelete';
         
         app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
