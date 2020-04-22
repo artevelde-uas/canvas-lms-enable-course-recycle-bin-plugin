fa5681cba2755b422f16d00ef490eb1e719fed27
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-04-22 14:06:05 +0200

    Use object deconstruction

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import translations from './i18n.json';
 
 
-export default function (app) {
-    const __ = app.i18n.translate;
-    
-    app.i18n.setTranslations(translations);
-    
-    app.addRouteListener('courses.settings', function (params) {
-        let url = app.getRouteUrl('courses.settings.undelete', {
+export default function ({ addRouteListener, getRouteUrl, addReadyListener, i18n, i18n: { translate: __ } }) {
+    i18n.setTranslations(translations);
+
+    addRouteListener('courses.settings', function (params) {
+        let url = getRouteUrl('courses.settings.undelete', {
             courseId: params.courseId
         });
-        
-        app.addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
+
+        addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
             el.insertAdjacentHTML('afterend', `
                 <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
                     <i class="icon-trash"></i>${__('recycle_bin')}
