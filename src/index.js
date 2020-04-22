31c5376692d93a9cf19a4704d2e8bb342325eed2
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-04-22 15:25:21 +0200

    Use new services

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import translations from './i18n.json';
 
 
-export default function ({ addRouteListener, getRouteUrl, addReadyListener, i18n, i18n: { translate: __ } }) {
+export default function ({ router, dom, i18n, i18n: { translate: __ } }) {
     i18n.setTranslations(translations);
 
-    addRouteListener('courses.settings', function (params) {
-        let url = getRouteUrl('courses.settings.undelete', {
+    router.onRoute('courses.settings', function (params) {
+        let url = router.getUrl('courses.settings.undelete', {
             courseId: params.courseId
         });
 
-        addReadyListener('#right-side > div:first-child > a:last-of-type', function (el) {
+        dom.onElementAdded('#right-side > div:first-child > a:last-of-type', function (el) {
             el.insertAdjacentHTML('afterend', `
                 <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
                     <i class="icon-trash"></i>${__('recycle_bin')}
                 </a>
             `);
-        });
+        }, { once: true });
     });
 }
