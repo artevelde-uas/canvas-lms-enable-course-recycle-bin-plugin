ca612154b9362303ab536351b622e3853058ae6b
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-05-13 11:30:20 +0200

    Use new ònElementReady method

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import translations from './i18n.json';
 export default function ({ router, dom, i18n, i18n: { translate: __ } }) {
     i18n.setTranslations(translations);
 
-    router.onRoute('courses.settings', function (params) {
+    router.onRoute('courses.settings', async params => {
         let url = router.getUrl('courses.settings.undelete', {
             courseId: params.courseId
         });
 
-        dom.onElementAdded('#right-side > div:first-child > a:last-of-type', function (el) {
-            el.insertAdjacentHTML('afterend', `
-                <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
-                    <i class="icon-trash"></i>${__('recycle_bin')}
-                </a>
-            `);
-        }, { once: true });
+        let el = await dom.onElementReady('#right-side > div:first-child > a:last-of-type');
+
+        el.insertAdjacentHTML('afterend', `
+            <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
+                <i class="icon-trash"></i>${__('recycle_bin')}
+            </a>
+        `);
     });
 }
