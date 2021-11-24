188d7b9f77a2c30c113779086c3265cbc2097eda
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-11-25 00:59:42 +0100

    Refactor variables

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import __ from './i18n';
 
 export default function () {
     router.onRoute('courses.settings', async params => {
-        let url = router.getUrl('courses.settings.undelete', {
+        const lastMenuItem = await dom.onElementReady('#right-side > div:first-child > a:last-of-type');
+        const url = router.getUrl('courses.settings.undelete', {
             courseId: params.courseId
         });
 
-        let el = await dom.onElementReady('#right-side > div:first-child > a:last-of-type');
-
-        el.insertAdjacentHTML('afterend', `
+        lastMenuItem.insertAdjacentHTML('afterend', `
             <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
                 <i class="icon-trash"></i>${__('recycle_bin')}
             </a>
