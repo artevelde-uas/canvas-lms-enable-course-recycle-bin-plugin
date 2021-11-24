4ca5451a03890f3cd82a7a6999281fb71ed477e6
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-11-25 00:59:57 +0100

    Add some comments

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import { router, dom } from '@artevelde-uas/canvas-lms-app';
 import __ from './i18n';
 
 
+/**
+ * Adds a 'Course Recycle Bin' link to the course settings menu
+ */
 export default function () {
     router.onRoute('courses.settings', async params => {
         const lastMenuItem = await dom.onElementReady('#right-side > div:first-child > a:last-of-type');
@@ -9,6 +12,7 @@ export default function () {
             courseId: params.courseId
         });
 
+        // Append the 'Course Recycle Bin' link to the menu on the right side
         lastMenuItem.insertAdjacentHTML('afterend', `
             <a class="Button Button--link Button--link--has-divider Button--course-settings" href="${url}">
                 <i class="icon-trash"></i>${__('recycle_bin')}
